f6eeb1c6d12ff341bb3948a19638cdc782f0d5c4
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-07-21 19:58:18 -0500

    Postman/insomnia setup and tests

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -3,10 +3,10 @@ const getAllTasks = (req, res) => {
 } 
 
 const createTasks = (req, res) => {
-    res.send('create task')
+    res.json(req.body)
 }
 const getTasks = (req, res) => {
-    res.send('get single task')
+    res.json({id:req.params.id})
 }
 const updateTasks = (req, res) => {
     res.send('update task')
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,9 +1,15 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
 
-const {getAllTasks, createTasks, getTasks, updateTasks, deleteTasks} = require('../controllers/tasks')
+const {
+  getAllTasks,
+  createTasks,
+  getTasks,
+  updateTasks,
+  deleteTasks,
+} = require("../controllers/tasks");
 
-router.route('/').get(getAllTasks).post(createTasks)
-router.route('/:id').get(getTasks).patch(updateTasks).delete(deleteTasks)
+router.route("/").get(getAllTasks).post(createTasks);
+router.route("/:id").get(getTasks).patch(updateTasks).delete(deleteTasks);
 
-module.exports = router
+module.exports = router;
