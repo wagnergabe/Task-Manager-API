d23f69f43182191dd6f751de0a875bfbe8ae9c2b
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-07-23 21:05:30 -0500

    installed mongoose, db connected

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('./db/connect')
+
 const express = require('express')
 const app = express();
 const tasks = require('./routes/tasks')
--- db/connect.js
+++ db/connect.js
@@ -1,1 +1,5 @@
-const connectionString = 'mongodb+srv://Gabriel:<password>@nodeexpressproject.mc8qao3.mongodb.net/?retryWrites=true&w=majority'
+const mongoose = require('mongoose')
+
+const connectionString = 'mongodb+srv://Gabriel:testtest123@nodeexpressproject.mc8qao3.mongodb.net/?retryWrites=true&w=majority'
+
+mongoose.connect(connectionString).then(() => console.log('CONNECTED TO THE DB')).catch((err) => console.log(err))
