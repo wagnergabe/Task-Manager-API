0d41c232ba64a443294f7e9f4a424901facc8226
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-07-23 21:07:53 -0500

    removed deprecation warnings

--- db/connect.js
+++ db/connect.js
@@ -2,4 +2,9 @@ const mongoose = require('mongoose')
 
 const connectionString = 'mongodb+srv://Gabriel:testtest123@nodeexpressproject.mc8qao3.mongodb.net/?retryWrites=true&w=majority'
 
-mongoose.connect(connectionString).then(() => console.log('CONNECTED TO THE DB')).catch((err) => console.log(err))
+mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+}).then(() => console.log('CONNECTED TO THE DB')).catch((err) => console.log(err))
