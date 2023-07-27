bae9b9b3f4b8d9c71e49616df2ec556885980c57
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-07-27 08:59:57 -0500

    delete task working

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -34,8 +34,18 @@ const getTask = async (req, res) => {
 const updateTasks = (req, res) => {
   res.send("update task");
 };
-const deleteTasks = (req, res) => {
-  res.send("delete task");
+const deleteTasks = async (req, res) => {
+    try{
+        const {id:taskID} = req.params;
+        const task = await Task.findOneAndDelete({_id:taskID});
+        if(!task) {
+            return res.status(404).json({msg: `No task with if : ${taskID}`})
+        }
+    res.status(200).json({task})
+    }catch(error) {
+        res.status(500).json({ msg: error })
+    }
+ 
 };
 
 module.exports = {
