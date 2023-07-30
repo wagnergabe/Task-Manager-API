3a4b83dc297a6bd181d3f6a848e508ec6119136a
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-07-29 20:54:41 -0500

    add front end/ fixed ip address in mongo

--- app.js
+++ app.js
@@ -5,11 +5,9 @@ const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 require('dotenv').config()
 
-app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('Task Manager App')
-});
+app.use(express.static('./public'))
+app.use(express.json())
 
 
 
