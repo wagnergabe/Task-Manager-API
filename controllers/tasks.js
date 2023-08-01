a4e6ff50b1fa2dfb43dbfca2d4b9f7f558a6c707
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-07-31 21:26:17 -0500

    add async wrapper to controller tasks

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,13 +1,11 @@
 const Task = require("../models/Task");
+const asyncWrapper = require('../middleware/async')
 
-const getAllTasks = async (req, res) => {
-    try{
+
+const getAllTasks = asyncWrapper(async (req, res) => {
         const tasks = await Task.find({})
         res.status(200).json({ tasks })
-    }catch(error){
-        res.status(500).json({ msg: "Could Not Find Task" })
-    }
-};
+});
 
 const createTasks = async (req, res) => {
   try {
