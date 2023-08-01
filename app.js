cc46e6c1698b4232da5e0ad55007fa2bdc9dae7a
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-07-31 21:19:53 -0500

    setup middleware

--- app.js
+++ app.js
@@ -3,15 +3,18 @@ const express = require('express')
 const app = express();
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
+const notFound = require('./middleware/not-found')
 require('dotenv').config()
 
-
+//middleware
 app.use(express.static('./public'))
 app.use(express.json())
 
 
-
+//routes
 app.use('/api/v1/tasks', tasks)
+
+app.use(notFound)
 
 const port = 3000
 
