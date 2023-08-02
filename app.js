d22a44f5c05e29c71f65052d3345e8726505fc88
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-08-01 21:31:55 -0500

    update middleware, custom error message

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 require('dotenv').config()
 
 //middleware
@@ -14,7 +15,8 @@ app.use(express.json())
 //routes
 app.use('/api/v1/tasks', tasks)
 
-app.use(notFound)
+app.use(notFound);
+app.use(errorHandlerMiddleware)
 
 const port = 3000
 
--- controllers/tasks.js
+++ controllers/tasks.js
@@ -7,30 +7,24 @@ const getAllTasks = asyncWrapper(async (req, res) => {
         res.status(200).json({ tasks })
 });
 
-const createTasks = async (req, res) => {
-  try {
+const createTasks = asyncWrapper(async(req, res) => {
+  
     const task = await Task.create(req.body);
     res.status(201).json({ task });
-  } catch (error) {
-    res.status(500).json({ error });
-  }
-};
+  })
 
-const getTask = async (req, res) => {
-    try {
+
+const getTask = asyncWrapper(async (req, res) => {
+    
         const { id:taskID } = req.params 
         const task = await Task.findOne({ _id:taskID })
         if(!task) {
             return res.status(404).json({msg: `No task with id : ${taskID}`})
         }
         res.status(200).json({ task })
-    } catch(error) {
-        res.status(500).json({ msg: "Could Not Find Task"})
-    }
-};
+});
 
-const updateTasks = async (req, res) => {
-  try{
+const updateTasks = asyncWrapper(async (req, res) => {
     const {id: taskID} = req.params
 
     const task = await Task.findOneAndModify({_id:taskID,}, req.body, {
@@ -40,11 +34,7 @@ const updateTasks = async (req, res) => {
     if (!task) {
         return res.status(404).json({ msg: 'No task with id :' + taskID})
     }
-
-  }catch(error) {
-    res.status(500).json({ msg: error})
-  }
-};
+});
 
 const deleteTasks = async (req, res) => {
     try{
